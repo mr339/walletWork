8eba61e05d2f329cac4c339e387a87d782aabd02
ek_mac_46 <ek_mac_46@Ganesh339.local>
2022-12-13 11:05:14 +0545

    New version and html changes

--- public/electron.js
+++ public/electron.js
@@ -97,17 +97,9 @@ autoUpdater.on('update-downloaded', (info) => {
 // This method will be called when Electron has finished its initialization and
 // is ready to create the browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+app.on('ready', function () {
   createWindow();
-  // setupLocalFilesNormalizerProxy();
 
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
 });
 
 // Quit when all windows are closed, except on macOS.
